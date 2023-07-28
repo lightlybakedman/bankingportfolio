dc419d14fb5565fe5205e847dcb1289d5dbb620a
lightlybakedman <didi.wang@outlook.com>
2023-07-28 21:08:38 +1000

    Added rebalancing

--- portfolio_objects.py
+++ portfolio_objects.py
@@ -1,76 +1,74 @@
 from interest_rates import get_central_bank_rate
 import random
+from copy import deepcopy
 
 percentage_to_decimal = lambda x: x / 100
+random_interest_rate = lambda: random.randrange(-100, 100, 1) / 1000
 
 
-class ConstantInterestAccount:
-    def __init__(
-        self, currency: str, initial_amount: int, country: str, starting_year: int
-    ):
+class Portfolio:
+    def __init__(self, accounts: list):
+        self.accounts = accounts
+
+    def calculate_balances(self):
+        for account in self.accounts:
+            account.calculate_balance()
+
+    def calculate_total_balance(self):
+        return sum([account.balance for account in self.accounts])
+
+    def rebalance(self):
+        total_balance = self.calculate_total_balance()
+        for account in self.accounts:
+            account.balance = total_balance / len(self.accounts)
+
+
+class RandomInterestAccount:
+    def __init__(self, currency: str, balance: int, country: str):
         self.currency = currency
-        self.initial_amount = initial_amount
+        self.balance = balance
         self.country = country
-        self.starting_year = starting_year
 
-        self.rate = get_central_bank_rate(country)
-
-    def calculate_balance(self, years: int):
-        balances = {self.starting_year: self.initial_amount}
-        for year in range(1, years + 1):
-            balances[self.starting_year + year] = (
-                self.initial_amount * (1 + percentage_to_decimal(self.rate)) ** year
-            )  # calculate the balance for each year
-        return balances
-
-
-class RandomAccount:
-    def __init__(
-        self, currency: str, initial_amount: int, country: str, starting_year: int
-    ):
-        self.currency = currency
-        self.initial_amount = initial_amount
-        self.country = country
-        self.starting_year = starting_year
-
-    def calculate_balance(self, years: int):
-        get_rate = (
-            lambda: random.randrange(-100, 100, 1) / 1000
-        )  # random interest rate from -10.0% to 10.0%
-        balances = {self.starting_year: self.initial_amount}
-        for year in range(1, years + 1):
-            balances[self.starting_year + year] = balances[
-                self.starting_year + year - 1
-            ] * (1 + get_rate())
-        return balances
+    def calculate_balance(self):
+        self.balance = self.balance * (1 + random_interest_rate())
 
 
 # test values
 initial_amount = 10000
-starting_year = 2023
-years = 20
 
-germany = ConstantInterestAccount("AUD", initial_amount, "Germany", starting_year)
-united_states = ConstantInterestAccount(
-    "AUD", initial_amount, "United States", starting_year
-)
-australia = ConstantInterestAccount("AUD", initial_amount, "Australia", starting_year)
-china = RandomAccount("AUD", initial_amount, "China", starting_year)
+germany = RandomInterestAccount("AUD", initial_amount, "Germany")
+united_states = RandomInterestAccount("AUD", initial_amount, "United States")
+australia = RandomInterestAccount("AUD", initial_amount, "Australia")
+china = RandomInterestAccount("AUD", initial_amount, "China")
 
 accounts = [germany, united_states, australia, china]
 
+initial_portfolio = Portfolio(accounts)
 
-# plotting
 
-import matplotlib.pyplot as plt
+def generate_portfolio_over_time(portfolio: Portfolio, years: int):
+    values = {0: portfolio}
+    for year in range(1, years):
+        portfolio.calculate_balances()
+        portfolio.rebalance()
+        print(f"Year {year + 1}: {portfolio.calculate_total_balance()}")
+        values[year] = deepcopy(portfolio)
 
-balances = [list(account.calculate_balance(20).values()) for account in accounts]
-years_list = list(range(starting_year, starting_year + years + 1))
 
-# print(balances)
-plt.plot(years_list, balances[0], label="Germany")
-plt.plot(years_list, balances[1], label="United States")
-plt.plot(years_list, balances[2], label="Australia")
-plt.plot(years_list, balances[3], label="China")
-# plt.axis([starting_year, starting_year + years, 0, 20000])
-plt.show()
+generate_portfolio_over_time(initial_portfolio, 20)
+
+# # plotting
+
+# import matplotlib.pyplot as plt
+
+# balances = [list(account.calculate_balance(20).values()) for account in accounts]
+# years_list = list(range(starting_year, starting_year + years + 1))
+
+# # print(balances)
+# plt.plot(years_list, balances[0], label="Germany")
+# plt.plot(years_list, balances[1], label="United States")
+# plt.plot(years_list, balances[2], label="Australia")
+# plt.plot(years_list, balances[3], label="China")
+# leg = plt.legend(loc="upper center")  # add labels to lines
+# # plt.axis([starting_year, starting_year + years, 0, 20000])
+# plt.show()
